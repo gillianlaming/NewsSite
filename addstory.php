4aacc9393908438d21d79d119992d3a223372440
Gillian Laming <gdlaming@wustl.edu>
2018-09-20 03:37:56 +0200

    addstory

---
+++ addstory.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML
+<html lang="en">
+	<head>
+		<!--add a story to the turnip-->
+		<meta charset="utf-8"/>
+		<title>The Turnip: Add a Story</title>
+		<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<link rel="stylesheet" href="../turnip.css">
+	</head>
+	<body>
+		<h1>Add a Story</h1>
+	
+	<!--give story a title-->
+	<form name="form" action="submit" method="post">
+		<input type="text" name="title" id="title" value="Title">
+		<input type="text" name="newstory" id="newstory" value="Add Story here">
+		<input type="submit" name="add_story" value="Add Story">
+	</form>
+	
+	<?php
+	require 'database.php';
+	
+	if(isset($_POST['add_story'])){ /*i do not think this is the right command*/
+	
+		//obtain user so that sotry can be credited to them
+		$name = 'leela';
+		//$_POST["name"]; /*gets username*/
+		$add_story = $mysqli->prepare("insert into stories (title, body, author) values(?, ?, ?)");
+		if(!$add_story){
+			printf("Query Prep Failed: %s\n", $mysqli->error);
+			exit;
+		}
+		
+		$add_story->bind_param('sss', $title, $newstory, $name); /*adds the title, body, and author*/
+		$add_story->execute();
+		$add_story->close();
+		
+		echo "good news! your story has been posted!";
+		echo "<a href=../theturnip.php>Home</a>";
+	}
+		//echo "<p style=\"width: 50%; margin-left: 25%;\"> <br>"; //styling
+	?>
+
+	<!--return back to turnip homepage-->
+	 <form action="../theturnip.php"> 
+    		<input type="button" onclick="location='../theturnip.php/<?php echo $enc ?>'" value="Return Home"><br>
+    </form>
+	</body>
+
+</html>
