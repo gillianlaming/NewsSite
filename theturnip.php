2cf68d40a65b3733891e4403c5c4869b8e45503e
Leela Ghaemmaghami <leela@Leelas-MacBook-Pro.local>
2018-09-22 17:31:42 -0400

    urls are here but don't work yet

--- theturnip.php
+++ theturnip.php
@@ -11,22 +11,22 @@
         <h1>THE TURNIP</h1>
         <p>america's favorite news source for all things fucked up &amp; funky</p>
         
-        <h2>Recent Stories: </h2>
+        <h2>Recent Stories</h2>
 
         <?php // display stories
             require 'database.php';
-            $see_story = $mysqli->prepare("select id, title, body, story_date, author from stories order by id");
+            $see_story = $mysqli->prepare("select id, title, body, story_date, author, url_id from stories order by id");
             if(!$see_story){
                 printf("Query Prep Failed: %s\n", $mysqli->error);
                 exit;
             }
             
             $see_story->execute();
-            $see_story->bind_result($story_id, $title, $body, $date, $author);
+            $see_story->bind_result($story_id, $title, $body, $date, $author, $url);
             
             echo "<ul id='stories'>\n";
             while($see_story->fetch()){
-                echo "<li><h4><a href='".$title."+".$story_id."'>".$title."</a>";
+                echo "<li><h4><a href='".$url."'>".$title."</a>";
                 echo " <i> by ".$author."</i></h4> <h6>".$date."</h6>";
                 echo $body. "<br><br><hr></li>";
             }
