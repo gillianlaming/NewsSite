e8779ea0b6e3ba96d428feb7f019ce29559a5932
Leela Ghaemmaghami <leela@leelas-mbp.dhcp.wustl.edu>
2018-09-25 22:43:43 -0500

    edi tpages pretty

--- edit_comment.php
+++ edit_comment.php
@@ -5,13 +5,24 @@
 	$url = $_POST['url'];
 	
 ?>
-	<form action = "" method="post">
-		Comment: <input type="text" id="commentbox" name="new_comment" value="<?php echo $comment; ?>"/><br/>
-		<input type="submit" id="submit" name="submit" value="Submit">
-		<input type="hidden" name="comment_id" value="<?php echo $comment_id ?>">
-		<input type="hidden" name="comment" value="<?php echo $comment ?>">
-		<input type="hidden" name="url" value="<?php echo $url ?>">
-	</form>
+	<head>
+		<meta charset="utf-8"/>
+        <title>Edit Comment</title>
+        <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <link rel="stylesheet" href="../turnip.css">
+	</head>
+	<body>
+		<br><br>
+		<h1>Edit Your Comment</h1>
+		<br>
+		<form action = "" method="post">
+			<input type="text" id="commentbox" name="new_comment" value="<?php echo $comment; ?>"/><br/>
+			<input type="submit" id="submit" name="submit" value="Submit">
+			<input type="hidden" name="comment_id" value="<?php echo $comment_id ?>">
+			<input type="hidden" name="comment" value="<?php echo $comment ?>">
+			<input type="hidden" name="url" value="<?php echo $url ?>">
+		</form>
+	</body>
 <?php
 
 	if (isset($_POST['submit'])){
--- edit_story.php
+++ edit_story.php
@@ -5,13 +5,24 @@
 	$url = $_POST['url'];
 	
 	?>
+	<head>
+		<meta charset="utf-8"/>
+        <title>Edit Story</title>
+        <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <link rel="stylesheet" href="../turnip.css">
+	</head>
+	<body>
+		<br><br>
+		<h1>Edit Story</h1>
+		<br>
 		<form action = "" method="post">
-			Body: <input type="text" id="newstory" name="new_story" value="<?php echo $body; ?>"/><br/>
+			<input type="text" id="newstory" name="new_story" value="<?php echo $body; ?>"/><br/>
 			<input type="submit" id="submit" name="submit" value="Update">
 			<input type="hidden" name="story_id" value="<?php echo $story_id ?>">
 			<input type="hidden" name="body" value="<?php echo $body ?>">
 			<input type="hidden" name="url" value="<?php echo $url ?>">
 		</form>
+	</body>
 <?php
 
 	if (isset($_POST['submit'])){
