429efa187629ffe96b99d0639d7e6092fb9d2f76
Gillian Laming <gdlaming@wustl.edu>
2018-09-26 03:42:59 +0200

    edit comment

--- edit_comment.php
+++ edit_comment.php
@@ -2,27 +2,38 @@
 	require ('database.php');
 	$comment_id = $_POST['comment_id'];
 	$comment=$_POST['comment'];
+	
 	?>
-			<form>
-			Comment: <input type="text" id="commentbox" name="new_comment" method="post" value="<?php echo $comment; ?>"/><br/>
+			<form action = "" method="post">
+			Comment: <input type="text" id="commentbox" name="new_comment" value="<?php echo $comment; ?>"/><br/>
 			<input type="submit" id="submit" name="submit" value="Submit">
+			<input type="hidden" name="comment_id" value="<?php echo $comment_id ?>">
+			<input type="hidden" name="comment" value="<?php echo $comment ?>">
 			</form>
 	<?php
-	
+
 	if (isset($_POST['submit'])){
-		echo "hey";
+		
 		$new_comment = $_POST['new_comment'];
-		$update = $mysqli->prepare("UPDATE comments SET comment= ? WHERE comment_id= ?");
+		$update = $mysqli->prepare("UPDATE comments SET comment = ? WHERE comment_id = ?");
 		if (!$update){
 			printf("Query Prep Failed: %s\n", $mysqli->error);
             exit;
 			}
-		echo "she worked";
+		
 		$update->bind_param('si', $new_comment, $comment_id);
 		$update->execute(); 
 		$update->close();
+		//$previous = "javascript:history.go(-3)";
+		//if(isset($_SERVER['HTTP_REFERER'])) {
+		//	$previous = $_SERVER['HTTP_REFERER'];
+		//}
 		
-		header('Location: ../theturnip.php');
+		
+		echo "<a href=\"javascript:history.go(-2)\">GO BACK</a>";
+		//header('Location: ../theturnip.php');
+		//header('Location: ' . $_SERVER['HTTP_REFERER']);
+		
 	}
 	
 	?>
